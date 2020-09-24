0022adf796a05c78f1e10dce9c47958f0aa7e684
Wirkus Tomasz <wirkus919@gmail.com>
2020-09-24 23:07:02 +0200

    Added nav svgs

---
+++ src/img/Svgs.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+
+export const HomeIcon = (props) => (
+  <svg viewBox='0 0 8 8' {...props}>
+    <path d='M4 0l-4 3h1v4h2v-2h2v2h2v-4.03l1 .03-4-3z'></path>
+  </svg>
+);
+export const SkillsIcon = (props) => (
+  <svg viewBox='0 0 8 8' {...props}>
+    <path
+      d='M5 0l-3 6h1l3-6h-1zm-4 1l-1 2 1 2h1l-1-2 1-2h-1zm5 0l1 2-1 2h1l1-2-1-2h-1z'
+      transform='translate(0 1)'
+    ></path>
+  </svg>
+);
+export const ProjectsIcon = (props) => (
+  <svg viewBox='0 0 8 8' {...props}>
+    <path d='M3 0c-.55 0-1 .45-1 1v1h-1.91c-.06 0-.09.04-.09.09v2.41c0 .28.22.5.5.5h7c.28 0 .5-.22.5-.5v-2.41c0-.06-.04-.09-.09-.09h-1.91v-1c0-.55-.45-1-1-1h-2zm0 1h2v1h-2v-1zm-3 4.91v2c0 .05.04.09.09.09h7.81c.05 0 .09-.04.09-.09v-2c-.16.06-.32.09-.5.09h-7c-.18 0-.34-.04-.5-.09z'></path>
+  </svg>
+);
+
+export const ContactIcon = (props) => (
+  <svg viewBox='0 0 8 8' {...props}>
+    <path
+      d='M0 0v1l4 2 4-2v-1h-8zm0 2v4h8v-4l-4 2-4-2z'
+      transform='translate(0 1)'
+    ></path>
+  </svg>
+);
