dfaddd412ae48e9bec4ee1f5d5ce3a8ca2f827a6
Wirkus Tomasz <wirkus919@gmail.com>
2020-09-25 18:13:59 +0200

    Animated skills component
    Also added link with more informations bellow

--- src/components/skills/Skills.tsx
+++ src/components/skills/Skills.tsx
@@ -1,7 +1,32 @@
 import React from 'react';
-import styled from 'styled-components';
+import styled, { keyframes } from 'styled-components';
 import { Heading } from '../Heading';
 import * as Icon from '../../img/Svgs';
+const Roll = require('react-reveal/Roll');
+const Fade = require('react-reveal/Fade');
+
+const starFakeHoverAnimation = keyframes`
+  0% {
+      fill: #aaa;
+  }
+  50% {
+      fill: yellow;
+  }
+  100% {
+    fill: #aaa;
+}
+`;
+const titleFakeHoverAnimation = keyframes`
+  0% {
+      color: #ccc;
+  }
+  50% {
+      color: white;
+  }
+  100% {
+    color: #ccc;
+}
+`;
 
 const SkillsWrapper = styled.section`
   width: 100%;
@@ -14,7 +39,6 @@ const Content = styled.div`
   justify-content: space-between;
   width: 75%;
 `;
-
 const TitleContainer = styled.div`
   display: flex;
   align-items: center;
@@ -29,18 +53,25 @@ const TitleContainer = styled.div`
     margin-right: 2.5em;
 
     transition: all 1.2s;
+
+    animation: ${starFakeHoverAnimation} 2s ease
+      ${(props: { delay: string }) => props.delay};
   }
   h3 {
     font-size: 3em;
     color: #ccc;
     letter-spacing: 3px;
     text-transform: uppercase;
+
+    animation: ${titleFakeHoverAnimation} 1s ease
+      ${(props: { delay: string }) => props.delay};
   }
 `;
 const ElementContainer = styled.div`
   display: flex;
   align-items: center;
   transition: all 0.5s;
+
   &:not(:last-of-type) {
     margin-bottom: 1em;
   }
@@ -94,89 +125,119 @@ const SkillsContainer = styled.div`
     }
   }
 `;
+const SPAN = styled.span`
+  width: 60%;
+  font-size: 2.25em;
+  color: #ccc;
+  font-weight: 100;
+  margin-top: 3em;
+  text-align: center;
+`;
+const A = styled.a`
+  &:visited,
+  &:link {
+    color: #fff;
+    font-weight: 100;
+  }
+`;
 
 export const Skills = () => {
   return (
     <SkillsWrapper id='skills'>
       <Heading text='Skills' />
       <Content>
-        <SkillsContainer>
-          <TitleContainer>
-            <Icon.StarIcon />
-            <h3>Front-end</h3>
-          </TitleContainer>
-          <ElementContainer fillColor='#1572b6'>
-            <Icon.CSSIcon />
-            <span>CSS | Sass | BEM</span>
-          </ElementContainer>
-          <ElementContainer fillColor='#61dafb'>
-            <Icon.ReactIcon />
-            <span>React</span>
-          </ElementContainer>
-          <ElementContainer fillColor='#764ABC'>
-            <Icon.ReduxIcon />
-            <span>Redux</span>
-          </ElementContainer>
-          <ElementContainer fillColor='#DB7093'>
-            <Icon.StyledComponentsIcon />
-            <span>Styled components</span>
-          </ElementContainer>
-          <ElementContainer fillColor='#CB3837'>
-            <Icon.NpmIcon />
-            <span>3rd party components</span>
-          </ElementContainer>
-        </SkillsContainer>
+        <Roll left>
+          <SkillsContainer>
+            <TitleContainer delay='1s'>
+              <Icon.StarIcon />
+              <h3>Front-end</h3>
+            </TitleContainer>
+            <ElementContainer fillColor='#1572b6'>
+              <Icon.CSSIcon />
+              <span>CSS | Sass | BEM</span>
+            </ElementContainer>
+            <ElementContainer fillColor='#61dafb'>
+              <Icon.ReactIcon />
+              <span>React</span>
+            </ElementContainer>
+            <ElementContainer fillColor='#764ABC'>
+              <Icon.ReduxIcon />
+              <span>Redux</span>
+            </ElementContainer>
+            <ElementContainer fillColor='#DB7093'>
+              <Icon.StyledComponentsIcon />
+              <span>Styled components</span>
+            </ElementContainer>
+            <ElementContainer fillColor='#CB3837'>
+              <Icon.NpmIcon />
+              <span>3rd party components</span>
+            </ElementContainer>
+          </SkillsContainer>
+        </Roll>
 
-        <SkillsContainer>
-          <TitleContainer>
-            <Icon.StarIcon />
-            <h3>Back-end</h3>
-          </TitleContainer>
-          <ElementContainer fillColor='#339933'>
-            <Icon.NodeJsIcon />
-            <span>Node.js</span>
-          </ElementContainer>
-          <ElementContainer fillColor='#000000'>
-            <Icon.ExpressIcon />
-            <span>Express</span>
-          </ElementContainer>
-          <ElementContainer fillColor='#47a248'>
-            <Icon.MongoDBIcon />
-            <span>MongoDB | Mongoose</span>
-          </ElementContainer>
-          <ElementContainer fillColor='#000000'>
-            <Icon.JWTIcon />
-            <span>JWT</span>
-          </ElementContainer>
-        </SkillsContainer>
+        <Fade bottom>
+          <SkillsContainer>
+            <TitleContainer delay='2.5s'>
+              <Icon.StarIcon />
+              <h3>Back-end</h3>
+            </TitleContainer>
+            <ElementContainer fillColor='#339933'>
+              <Icon.NodeJsIcon />
+              <span>Node.js</span>
+            </ElementContainer>
+            <ElementContainer fillColor='#000000'>
+              <Icon.ExpressIcon />
+              <span>Express</span>
+            </ElementContainer>
+            <ElementContainer fillColor='#47a248'>
+              <Icon.MongoDBIcon />
+              <span>MongoDB | Mongoose</span>
+            </ElementContainer>
+            <ElementContainer fillColor='#000000'>
+              <Icon.JWTIcon />
+              <span>JWT</span>
+            </ElementContainer>
+          </SkillsContainer>
+        </Fade>
 
-        <SkillsContainer>
-          <TitleContainer>
-            <Icon.StarIcon />
-            <h3>Tools&Others</h3>
-          </TitleContainer>
-          <ElementContainer fillColor='#f05032'>
-            <Icon.GitIcon />
-            <span>Git</span>
-          </ElementContainer>
-          <ElementContainer fillColor='#007ACC'>
-            <Icon.TypescriptIcon />
-            <span>TypeScript</span>
-          </ElementContainer>
-          <ElementContainer fillColor='#8DD6F9'>
-            <Icon.WebpackIcon />
-            <span>Webpack | npm | yarn</span>
-          </ElementContainer>
-          <ElementContainer fillColor='#000000'>
-            <Icon.AWSIcon />
-            <span>AWS EC2 | S3</span>
-          </ElementContainer>
-          <ElementContainer fillColor='#430098'>
-            <Icon.HerokuIcon />
-            <span>Heroku</span>
-          </ElementContainer>
-        </SkillsContainer>
+        <Roll right>
+          <SkillsContainer>
+            <TitleContainer delay='4s'>
+              <Icon.StarIcon />
+              <h3>Tools&Others</h3>
+            </TitleContainer>
+            <ElementContainer fillColor='#f05032'>
+              <Icon.GitIcon />
+              <span>Git</span>
+            </ElementContainer>
+            <ElementContainer fillColor='#007ACC'>
+              <Icon.TypescriptIcon />
+              <span>TypeScript</span>
+            </ElementContainer>
+            <ElementContainer fillColor='#8DD6F9'>
+              <Icon.WebpackIcon />
+              <span>Webpack | npm | yarn</span>
+            </ElementContainer>
+            <ElementContainer fillColor='#000000'>
+              <Icon.AWSIcon />
+              <span>AWS EC2 | S3</span>
+            </ElementContainer>
+            <ElementContainer fillColor='#430098'>
+              <Icon.HerokuIcon />
+              <span>Heroku</span>
+            </ElementContainer>
+          </SkillsContainer>
+        </Roll>
       </Content>
+      <SPAN>
+        Full list of technologies I work with you can find by clicking this{' '}
+        <A
+          href='https://docs.google.com/document/d/1UUktoP11nRqgAhTEppao9B58RL6dvOz4u9B0DO5Lt74/edit?usp=sharing'
+          target='_blanc'
+        >
+          link.
+        </A>
+      </SPAN>
     </SkillsWrapper>
   );
 };
