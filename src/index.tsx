00b0d9973ae1c0084f316b687b9740eba9a0c5b7
Wirkus Tomasz <wirkus919@gmail.com>
2020-09-28 15:11:22 +0200

    Resolved issue with TS and react-intl modules

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,14 @@
 import React from 'react';
+import Wrapper from './components/languages/Wrapper';
 import ReactDOM from 'react-dom';
 import './index.css';
 import { App } from './components/App';
 
 ReactDOM.render(
   <React.Fragment>
-    <App />
+    <Wrapper>
+      <App />
+    </Wrapper>
   </React.Fragment>,
   document.getElementById('root')
 );
