a36e01a7010347ed76ccfffa5f90b5a4a80ea0f5
Wirkus Tomasz <wirkus919@gmail.com>
2020-09-29 16:33:45 +0200

    Created design for profile page
    However I am not very happy from the outcome and I am thinking of moving
    the profile page content to welcome page.

--- src/components/profile/Profile.tsx
+++ src/components/profile/Profile.tsx
@@ -1,30 +1,68 @@
 import React from 'react';
 import styled from 'styled-components';
 import { FormattedMessage } from 'react-intl';
-import { Page, Info, Text, A } from '../layout/defaultStyles';
+import { Page, Info, Text, A, showUp } from '../layout/defaultStyles';
+import { Github, LinkedIn, Resume } from '../../img/Svgs';
 const Me = require('../../img/tw.png');
+const PlLang = require('../../img/pl.png');
+const EnLang = require('../../img/en.png');
 
 const ProfileContainer = styled.div`
   display: flex;
   justify-content: space-between;
-  width: 50%;
+  align-items: center;
+  width: 40rem;
   margin-top: -2rem;
 `;
 const DataContainer = styled.div`
+  width: calc(100% - 15rem);
+  height: 100%;
+
   display: flex;
   flex-direction: column;
   align: items-center;
-  width: calc(100% - 20rem);
 `;
 const SingleData = styled.div`
   display: flex;
   align-items: center;
+
+  &:not(:last-of-type) {
+    margin-bottom: 8px;
+  }
 `;
 const SingleDataName = styled.span`
-  width: 15rem;
+  width: 8rem;
   font-size: 1.5rem;
   color: #ccc;
   font-weight: 100;
+  animation: ${showUp} 1s ease-out 0.5s both;
+`;
+const SingleDataAnswerFlex = styled.div`
+  display: flex;
+  align-items: center;
+`;
+const LangImg = styled.img`
+  height: 1.2rem;
+  width: 1.2rem;
+  margin: 0 3px;
+  animation: ${showUp} 1s ease-out 1s both;
+
+  &:last-of-type {
+    margin-left: 10px;
+  }
+`;
+const Link = styled.a`
+  margin-top: 2px;
+  animation: ${showUp} 1s ease-out 1s both;
+
+  svg {
+    margin-right: 1rem;
+
+    &:first-of-type {
+      margin-left: 3px !important;
+      margin-right: calc(1rem - 3px);
+    }
+  }
 `;
 const ImageContainer = styled.div`
   width: 15rem;
@@ -44,7 +82,59 @@ export const Profile = () => {
             <SingleDataName>Name:</SingleDataName>
             <Text>Tomasz</Text>
           </SingleData>
+          <SingleData>
+            <SingleDataName>Surname:</SingleDataName>
+            <Text>Wirkus</Text>
+          </SingleData>
+          <SingleData>
+            <SingleDataName>Place of live:</SingleDataName>
+            <Text>Koszalin</Text>
+          </SingleData>
+          <SingleData>
+            <SingleDataName>Status:</SingleDataName>
+            <Text>Student of computer science</Text>
+          </SingleData>
+          <SingleData>
+            <SingleDataName>Email:</SingleDataName>
+            <Text>wirkus919@gmail.com</Text>
+          </SingleData>
+          <SingleData>
+            <SingleDataName>Languages:</SingleDataName>
+            <SingleDataAnswerFlex>
+              <LangImg src={PlLang} alt='pl lang' /> <Text> - Native,</Text>{' '}
+              <LangImg src={EnLang} alt='en lang' />
+              <Text> - B2</Text>
+            </SingleDataAnswerFlex>
+          </SingleData>
+
+          <SingleData>
+            <SingleDataName>Links:</SingleDataName>
+            <SingleDataAnswerFlex>
+              <Link
+                href='https://github.com/look919'
+                target='_blank'
+                rel='noopener noreferrer'
+              >
+                <Github />
+              </Link>
+              <Link
+                href='https://www.linkedin.com/in/tomasz-wirkus-435621196/'
+                target='_blank'
+                rel='noopener noreferrer'
+              >
+                <LinkedIn />
+              </Link>
+              <Link
+                href='https://drive.google.com/file/d/18zFQPB6Xhl6tQjVtKN8mUvSQQdtbFn5U/view'
+                target='_blank'
+                rel='noopener noreferrer'
+              >
+                <Resume />
+              </Link>
+            </SingleDataAnswerFlex>
+          </SingleData>
         </DataContainer>
+
         <ImageContainer>
           <Image src={Me} alt='Tomasz Wirkus' />
         </ImageContainer>
