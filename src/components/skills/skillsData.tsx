ca0a94dc30cb27dc8a2b60cb15822c25e4488e4f
Wirkus Tomasz <wirkus919@gmail.com>
2020-12-23 13:00:29 +0100

    Updated technologies list

--- src/components/skills/skillsData.tsx
+++ src/components/skills/skillsData.tsx
@@ -1,10 +1,11 @@
-import * as Icon from '../../img/Svgs';
 import React from 'react';
+import * as Icon from '../../img/Svgs';
 
 export const frontendData = [
   { icon: <Icon.CSSIcon />, fillColor: '#1572b6', text: 'CSS | Sass | BEM' },
   { icon: <Icon.ReactIcon />, fillColor: '#61dafb', text: 'React' },
   { icon: <Icon.ReduxIcon />, fillColor: '#764ABC', text: 'Redux' },
+  { icon: <Icon.MaterialUIIcon />, fillColor: '#0081CB', text: 'Material UI' },
   {
     icon: <Icon.StyledComponentsIcon />,
     fillColor: '#DB7093',
@@ -24,24 +25,15 @@ export const backendData = [
     fillColor: '#47a248',
     text: 'MongoDB | Mongoose',
   },
-  {
-    icon: <Icon.JWTIcon />,
-    fillColor: '#000',
-    text: 'JWT',
-  },
+  { icon: <Icon.JWTIcon />, fillColor: '#000', text: 'JWT' },
+  { icon: <Icon.HerokuIcon />, fillColor: '#430098', text: 'Heroku' },
+  { icon: <Icon.AWSIcon />, fillColor: '#000000', text: 'AWS EC2 | S3' },
 ];
 export const toolsData = [
   { icon: <Icon.GitIcon />, fillColor: '#f05032', text: 'Git' },
   { icon: <Icon.TypescriptIcon />, fillColor: '#007ACC', text: 'TypeScript' },
+  { icon: <Icon.FigmaIcon />, fillColor: '#F24E1E', text: 'Figma' },
   { icon: <Icon.WebpackIcon />, fillColor: '#8DD6F9', text: 'Webpack' },
-  {
-    icon: <Icon.AWSIcon />,
-    fillColor: '#000000',
-    text: 'AWS EC2 | S3',
-  },
-  {
-    icon: <Icon.HerokuIcon />,
-    fillColor: '#430098',
-    text: 'Heroku',
-  },
+  { icon: <Icon.PostmanIcon />, fillColor: '#FF6C37', text: 'Postman' },
+  { icon: <Icon.JiraIcon />, fillColor: '#0081CB', text: 'Scrum & Jira' },
 ];
